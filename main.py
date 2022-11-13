eae5cee7e897a3e8313827d3a1339b32ac55c3df
Joshua Leong <90120658+JoshuaLSE@users.noreply.github.com>
2022-11-13 18:21:58 +0800

    RabbitMQ, Converted to Class

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
 from pipelines import pipeline
+import pika
 
-if __name__ == '__main__':
-    # nlp = pipeline("question-generation")
-    nlp = pipeline("e2e-qg")  # Use this pipeline instead
-    text = "All that is gold does not glitter,Not all those who wander are lost,The old that is strong does not " \
-           "wither,Deep roots are not reached by the frost. From the ashes a fire shall be woken, A light from the " \
-           "shadows shall spring; Renewed shall be blade that was broken, The crownless again shall be king. "
-    print(nlp(text))
+# !/usr/bin/env python
+import pika
+import time
+
+
+class QuestionGen:
+    def __init__(self):
+        # Define Pipeline
+        self.nlp = pipeline("e2e-qg")
+
+    def processText(self, text):
+        """
+        Process and print the stored text
+        """
+        print(self.nlp(text))
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+QG = QuestionGen()
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body.decode())
+    QG.processText(body.decode())
+    print(" [x] Done")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
